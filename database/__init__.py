569fcdab5dcd7484d5c055c8d22689ac7d98974e
Todd Federman <tfederman@hotmail.com>
2025-01-27 19:44:55 -0500

    update feed fetch script

--- database/__init__.py
+++ database/__init__.py
@@ -9,11 +9,14 @@ class BaseModel(peewee.Model):
     class Meta:
         database = db
 
+
 class Feed(BaseModel):
     uri = peewee.CharField(unique=True)
-    added_by = peewee.CharField(null=True)
     added_at = peewee.DateTimeField(default=datetime.utcnow)
     title = peewee.CharField(null=True)
+    subtitle = peewee.CharField(null=True)
+    site_href = peewee.CharField(null=True)
+
 
 class Fetch(BaseModel):
     feed = peewee.ForeignKeyField(Feed)
@@ -30,21 +33,30 @@ class Fetch(BaseModel):
     etag_sent = peewee.CharField(null=True)
     modified_sent = peewee.CharField(null=True)
 
+
 class Article(BaseModel):
     fetch = peewee.ForeignKeyField(Fetch)
-    #link = peewee.CharField() # add this
+    link = peewee.CharField()
     title = peewee.CharField(null=True)
+    entry_id = peewee.CharField(null=True, unique=True)
     summary = peewee.CharField(null=True)
     author = peewee.CharField(null=True)
     tags = peewee.CharField(null=True)
     updated = peewee.DateTimeField(null=True)
-    site_unique_id = peewee.CharField(null=True, unique=True)
-    timestamp = peewee.CharField(null=True)
-    timestamp_parsed = peewee.DateTimeField(null=True)
+    updated_parsed = peewee.DateTimeField(null=True)
+    published = peewee.CharField(null=True)
+    published_parsed = peewee.DateTimeField(null=True)
+
+
+class ArticlePost(BaseModel):
+    article = peewee.ForeignKeyField(Article)
+    posted_at = peewee.DateTimeField(default=datetime.utcnow)
+    post_url = peewee.CharField(null=True)
+    exception = peewee.CharField(null=True)
 
 
 if __name__=="__main__":
 
     if db.is_closed():
         db.connect()
-        db.create_tables([Feed, Fetch, Article])
+        db.create_tables([Feed, Fetch, Article, ArticlePost])
--- feeds/fetch.py
+++ feeds/fetch.py
@@ -1,11 +1,14 @@
+import hashlib
 import datetime
 import time
+import json
 
 import feedparser
 feedparser.USER_AGENT = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.0.0 Safari/537.36"
 
 from database import *
 
+EARLIEST_DATE = datetime(2025, 1, 1)
 
 def fetch_and_save_feed(feed):
     try:
@@ -29,6 +32,32 @@ def fetch_and_save_feed(feed):
         fetch.exception = f"{type(e)} - {str(e)}"
         fp = None
 
+    try:
+        if fp.feed.title != feed.title:
+            feed.title = fp.feed.title
+            feed.save()
+    except:
+        pass
+
+    try:
+        if fp.feed.subtitle != feed.subtitle:
+            feed.subtitle = fp.feed.subtitle
+            feed.save()
+    except:
+        pass
+
+    try:
+        link_href = None
+        for link in fp.feed.links:
+            if link["rel"] == "alternate" and link["type"] == "text/html":
+                link_href = link["href"]
+                break
+        if link_href and link_href != feed.site_href:
+            feed.site_href = link_href
+            feed.save()
+    except:
+        pass
+
     for field in ["etag","modified","modified_parsed","href","updated","updated_parsed","version","status"]:
         if hasattr(fp, field):
             val = getattr(fp, field)
@@ -46,42 +75,57 @@ def save_articles(fetch, fp):
 
     articles_saved = 0
     for entry in fp.entries:
-        
+
+        try:
+            if datetime.fromtimestamp(time.mktime(entry.updated_parsed)) < EARLIEST_DATE:
+                #print(f"skip {datetime.fromtimestamp(time.mktime(entry.updated_parsed))}")
+                continue
+        except Exception as e:
+            pass
+
         if not hasattr(entry, "id"):
-            entry.id = hashlib.sha1(entry.link.encode('utf-8')).hexdigest()
+            try:
+                entry.id = hashlib.sha1(entry.link.encode('utf-8')).hexdigest()
+            except AttributeError:
+                entry.id = hashlib.sha1(str(entry).encode('utf-8')).hexdigest()
         
-        articles = Article.select().where(Article.site_unique_id==entry.id)
+        articles = Article.select().where(Article.entry_id==entry.id)
         if len(articles) > 0:
             continue
 
-        article = Article(fetch=fetch, site_unique_id=entry.id)
+        article = Article(fetch=fetch, entry_id=entry.id)
 
-        for field in ["title","summary","author","updated"]:
+        for field in ["title","summary","author","updated","updated_parsed","link","published","updated"]:
             if hasattr(entry, field):
                 val = getattr(entry, field)
 
                 if isinstance(val, time.struct_time):
-                    val = datetime.fromtimestamp(time.mktime(val))
+                    try:
+                        val = datetime.fromtimestamp(time.mktime(val))
+                    except ValueError:
+                        val = None
                 elif isinstance(val, str):
                     val = val[0:512]
 
                 setattr(article, field, val)
-        
+
         try:
-            article.updated = entry.updated
+            article.updated_parsed = datetime.fromtimestamp(time.mktime(entry.updated_parsed))
         except:
             pass
 
         try:
-            article.timestamp = entry.published
-            article.timestamp_parsed = datetime.fromtimestamp(time.mktime(entry.published_parsed))
+            article.published_parsed = datetime.fromtimestamp(time.mktime(entry.published_parsed))
         except:
             pass
 
         try:
-            article.tags = ', '.join(t['term'] for t in entry.tags)
+            article.tags = json.dumps([t['term'] for t in entry.tags[:16]])
         except:
             pass
+
+        if not article.link:
+            article.link = "(none)"
 
         article.save()
         articles_saved += 1
@@ -94,14 +138,17 @@ if __name__=='__main__':
     if db.is_closed():
         db.connect()
 
-    # feeds = list(Feed.select())
+    feeds = list(Feed.select())
 
     # feeds that have never been fetched
-    feeds = list(Feed.select().join(Fetch, peewee.JOIN.LEFT_OUTER, on=(Feed.id == Fetch.feed_id)).where(Fetch.id==None))
+    #feeds = list(Feed.select().join(Fetch, peewee.JOIN.LEFT_OUTER, on=(Feed.id == Fetch.feed_id)).where(Fetch.id==None))
+
+    import random
+    random.shuffle(feeds)
 
     # to do - daily, weekly logic etc.
 
-    for n,feed in enumerate(feeds):
+    for n,feed in enumerate(feeds[:10]):
         print(f"{n:04}/{len(feeds):04} {feed.uri}")
 
         fetch, fp = fetch_and_save_feed(feed)
