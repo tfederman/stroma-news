74b8d61c874258ab87d23021c8db30239283489e
Todd Federman <tfederman@hotmail.com>
2025-01-30 17:50:00 -0500

    reduce cardy lookups

--- media/card.py
+++ media/card.py
@@ -5,30 +5,30 @@ import requests
 from utils.string import html_to_text
 
 
-def get_post(session, feed_title, title, link, description, published_parsed, author):
+def get_post(session, article):
 
-    title = html_to_text(title)
-    embed = get_link_card_embed(session, link, title, description)
+    article.title = html_to_text(article.title)
+    embed = get_link_card_embed(session, article):
 
     text = []
 
-    if feed_title:
-        text.append(f'Feed: "{feed_title}"')
+    if article.fetch.feed.title:
+        text.append(f'Feed: "{article.fetch.feed.title}"')
 
-    if author:
-        author = author.replace("(noreply@blogger.com)", "").strip()
-        if "unknown" in author.lower():
-            author = ""
-        author = html_to_text(author)
+    if article.author:
+        article.author = article.author.replace("(noreply@blogger.com)", "").strip()
+        if "unknown" in article.author.lower():
+            article.author = ""
+        article.author = html_to_text(article.author)
 
     timestamp_format = "%A, %B %-d, %Y"
 
-    if author and published_parsed:
-        text.append(f'By: {author} on {published_parsed.strftime(timestamp_format)}')
-    elif author and not published_parsed:
-        text.append(f'By: {author}')
-    elif published_parsed and not author:
-        text.append(f'Published on {published_parsed.strftime(timestamp_format)}')
+    if article.author and article.published_parsed:
+        text.append(f'By: {article.author} on {article.published_parsed.strftime(timestamp_format)}')
+    elif article.author and not article.published_parsed:
+        text.append(f'By: {article.author}')
+    elif article.published_parsed and not article.author:
+        text.append(f'Published on {article.published_parsed.strftime(timestamp_format)}')
 
     post = {
         "$type": "app.bsky.feed.post",
@@ -50,25 +50,31 @@ def get_cardy_data(url):
         return {}
 
 
-def get_link_card_embed(session, url, title, description):
+def get_link_card_embed(session, article):
 
-    cardy_data = get_cardy_data(url)
+    try:
+        img_url = article.articlemeta_set[0].og_image or article.articlemeta_set[0].twitter_image
+        description = article.articlemeta_set[0].og_description or article.articlemeta_set[0].twitter_description
+    except:
+        img_url = None
+        description = None
 
-    img_url = cardy_data.get("image")
-    cardy_description = cardy_data.get("description")
-    if cardy_description:
-        description = cardy_description
-    else:
-        description = html_to_text(description)
+    if not img_url or not description:
+        cardy_data = get_cardy_data(article.link)
+        img_url = cardy_data.get("image")
+        description = cardy_data.get("description")
 
-    card = {"uri": url, "title": title, "description": description}
+    if not description:
+        description = html_to_text(article.summary)
+
+    card = {"uri": article.link, "title": article.title, "description": description}
 
     if img_url:
         try:
-            mimetype = get_mimetype(url)
+            mimetype = get_mimetype(img_url)
             r = requests.get(img_url)
             r.raise_for_status()
-            assert len(r.content) <= 999424, f"thumbnail image too big: ({len(r.content)}) {img_url} - {url}"
+            assert len(r.content) <= 999424, f"thumbnail image too big: ({len(r.content)}) {img_url} - {article.link}"
             upload_response = session.upload_file(r.content, mimetype)
             card["thumb"] = {
                 '$type': 'blob', 
