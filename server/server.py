8d1adf74930e726e1be499870625a48efe3c2e6a
Todd Federman <tfederman@hotmail.com>
2025-02-04 10:07:58 -0500

    add lambda serverless code

--- server/server.py
+++ server/server.py
@@ -11,3 +11,13 @@ dispatch_map = {
 def lambda_handler(event, context):
     path = event.get('rawPath') or 'default'
     return dispatch_map.get(path, default)(event)
+
+"""
+Notes:
+
+to update function code:
+- cd server && zip bsky-server.zip *.py && aws lambda update-function-code --function-name bsky_server --zip-file fileb://bsky-server.zip
+
+to update function config:
+- aws lambda update-function-configuration --function-name bsky_server --handler server.lambda_handler
+"""
