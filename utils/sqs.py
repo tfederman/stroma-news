59db9789cda6b58bd9c063275c482ac8c0733733
Todd Federman <tfederman@hotmail.com>
2025-02-06 17:54:44 -0500

    sqs script

--- utils/sqs.py
+++ utils/sqs.py
@@ -8,18 +8,27 @@ SUCCESS_QUEUE_URL = os.environ["SUCCESS_QUEUE_URL"]
 if __name__=="__main__":
 
     sqs = boto3.client('sqs')
+    f = open("sqs.log", "a")
 
-    response = sqs.receive_message(QueueUrl=SUCCESS_QUEUE_URL,
-                    MaxNumberOfMessages=10,
-                    MessageAttributeNames=['did','feed','post_count'])
+    fields = ['did','feed','limit','cursor','items_sent']
 
-    # entries for deletion
-    Entries = []
+    while True:
+        response = sqs.receive_message(QueueUrl=SUCCESS_QUEUE_URL,
+                        MaxNumberOfMessages=10,
+                        MessageAttributeNames=fields)
 
-    for msg in response['Messages']:
-        # do something
-        Entries.append({'Id': msg['MessageId'], 'ReceiptHandle': msg['ReceiptHandle']})
+        if not response.get('Messages'):
+            break
 
-    response = sqs.delete_message_batch(QueueUrl=SUCCESS_QUEUE_URL, Entries=Entries)
+        # entries for deletion
+        Entries = []
 
-    assert response["HTTPStatusCode"], f"sqs delete failure: {response['HTTPStatusCode']}"
+        for msg in response['Messages']:
+            print(",".join([str(msg['MessageAttributes'].get(f,{}).get('StringValue','')) for f in fields]))
+            f.write(",".join([str(msg['MessageAttributes'].get(f,{}).get('StringValue','')) for f in fields]) + "\n")
+            Entries.append({'Id': msg['MessageId'], 'ReceiptHandle': msg['ReceiptHandle']})
+
+        response = sqs.delete_message_batch(QueueUrl=SUCCESS_QUEUE_URL, Entries=Entries)
+        assert response["ResponseMetadata"]["HTTPStatusCode"] == 200, f"sqs delete failure: {response['HTTPStatusCode']}"
+
+    f.close()
