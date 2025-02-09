8a11ab25390614e707a838761fbf1540e7037db5
Todd Federman <tfederman@hotmail.com>
2025-02-09 10:47:55 -0500

    update mailbox script

--- mailbox/fetch.py
+++ mailbox/fetch.py
@@ -4,6 +4,8 @@ import peewee
 
 from bsky import session
 from database.models import BskyUserProfile, ConvoMessage
+from settings import log
+
 
 def get_and_save_messages(session):
 
@@ -13,20 +15,20 @@ def get_and_save_messages(session):
     received_at = datetime.now()
     counter = 0
 
-    for log in convo_logs.logs:
-        event_type = getattr(log, "$type")
+    for convo_log in convo_logs.logs:
+        event_type = getattr(convo_log, "$type")
         if event_type == "chat.bsky.convo.defs#logCreateMessage":
-            profile = BskyUserProfile.get_or_create_from_api(log.message.sender.did, session)
+            profile = BskyUserProfile.get_or_create_from_api(convo_log.message.sender.did, session)
 
             # if the cursor decorator on get_convo_logs is working correctly, each message
             # should only be returned by the API once which will avoid integrity errors
-            cm = ConvoMessage.create(message_id=log.message.id, convo_id=log.convoId,
-                    sender_did=log.message.sender.did, sender=profile, text=log.message.text,
-                    sent_at=log.message.sentAt, received_at=received_at)
+            facet_link = ConvoMessage.get_facet_link(convo_log.message)
+            cm = ConvoMessage.create(message_id=convo_log.message.id, convo_id=convo_log.convoId,
+                    sender_did=convo_log.message.sender.did, sender=profile, text=convo_log.message.text,
+                    sent_at=convo_log.message.sentAt, received_at=received_at, facet_link=facet_link)
             counter += 1
 
     return counter
-
 
 if __name__=="__main__":
     get_and_save_messages(session)
