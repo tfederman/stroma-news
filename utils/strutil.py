f5d1edbe348f31d83680b3fd6233ec1d1755fc45
Todd Federman <tfederman@hotmail.com>
2025-02-17 17:00:12 -0500

    add is_likely_binary function

--- utils/strutil.py
+++ utils/strutil.py
@@ -1,5 +1,8 @@
+from collections import Counter
+
 from bs4 import BeautifulSoup
 
+COMMON_TEXT_CHARS = set(range(32, 127)).union(set(ord(c) for c in '\n\r\t')).union({226,128,147,148,145,146})
 
 def html_to_text(s):
 
@@ -8,3 +11,7 @@ def html_to_text(s):
 
     soup = BeautifulSoup(s, 'html.parser')
     return soup.get_text(strip=False)
+
+def is_likely_binary(data):
+    c = Counter(char in COMMON_TEXT_CHARS for char in data)
+    return c[False] / len(data) > 0.10
