a7877b015fe2954cf451230b8e7617e2fcae67fc
Todd Federman <tfederman@hotmail.com>
2025-02-20 11:34:21 -0500

    try fetching image directly if cardy has an error

--- utils/image.py
+++ utils/image.py
@@ -1,9 +1,11 @@
 import io
 import math
+import urllib
 
 import requests
 from PIL import Image
 
+from settings import log
 from utils.strutil import is_likely_binary
 from utils.http import get_http_headers, ACCEPT_TYPE_DEFAULT, ACCEPT_TYPE_IMAGES
 
@@ -42,6 +44,13 @@ def get_http_image(url, accept_type=ACCEPT_TYPE_DEFAULT):
 
     mimetype = get_mimetype(url)
     r = requests.get(url, headers=get_http_headers(accept_type))
+
+    if url.startswith("https://cardyb.bsky.app/v1/image?url=") and r.status_code == 400:
+        target_url = url.replace("https://cardyb.bsky.app/v1/image?url=", "")
+        target_url = urllib.parse.unquote(target_url)
+        log.warning(f'cardy returned 400 for image "{url}", trying "{target_url}"')
+        return get_http_image(target_url)
+
     r.raise_for_status()
 
     if accept_type == ACCEPT_TYPE_DEFAULT and not is_likely_binary(r.content):
