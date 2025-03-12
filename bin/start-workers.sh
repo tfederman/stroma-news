f09ad30ca5685469e988f41752a3583831eefccc
Todd Federman <tfederman@hotmail.com>
2025-03-12 10:56:40 -0400

    rename systemd service

--- bin/start-workers.sh
+++ bin/start-workers.sh
@@ -3,7 +3,7 @@ sudo systemctl start rqworker-fetch@1
 sudo systemctl start rqworker-fetch@2
 sudo systemctl start rqworker-fetch@3
 
-sudo systemctl start rqworker-post@1
+sudo systemctl start rqworker-post
 
 sudo systemctl start rqworker-mailbox
 
--- bin/stop-workers.sh
+++ bin/stop-workers.sh
@@ -2,6 +2,6 @@ sudo systemctl stop rqworker-fetch@1
 sudo systemctl stop rqworker-fetch@2
 sudo systemctl stop rqworker-fetch@3
 
-sudo systemctl stop rqworker-post@1
+sudo systemctl stop rqworker-post
 
 sudo systemctl stop rqworker-mailbox
