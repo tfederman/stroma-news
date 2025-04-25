512f5edf4ab8bcbaa97ec7b0b6ad7b204ec662f3
Todd Federman <tfederman@hotmail.com>
2025-04-25 10:18:23 -0400

    misc scripts

---
+++ bin/check-labels.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+
+import psycopg2
+
+from pysky import BskyClient, APIError
+
+con = psycopg2.connect()
+cursor = con.cursor()
+
+bsky = BskyClient()
+feed = bsky.get_author_feed(actor=bsky.did, cursor=None, page_count=50)
+posts = [p.post for p in feed.feed if p.post.labels]
+
+for p in posts:
+    fields = [
+        p.uri.split("/")[-1],
+        p.labels[0].val,
+        p.embed.external.uri,
+        p.embed.external.title,
+        p.record.createdAt,
+        False,
+    ]
+    
+    placeholders = ",".join("%s" for _ in fields)
+    try:
+        cursor.execute(f"insert into labels values({placeholders})", fields)
+        con.commit()
+    except psycopg2.IntegrityError:
+        con.rollback()
+        continue
+
+con.close()
---
+++ bin/create-random-feeds.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+
+import time
+import json
+import subprocess
+from datetime import datetime, timedelta, UTC
+
+from peewee import fn
+
+from settings import log, S3_BUCKET, S3_PREFIX, bsky
+from database.models import Article, ArticlePost
+
+expressions = [
+    ArticlePost.post_id.is_null(False),
+    ArticlePost.deleted.is_null(),
+    ArticlePost.posted_at >= datetime.now(UTC) - timedelta(hours=24),
+    ArticlePost.posted_at <= datetime.now(UTC) + timedelta(hours=4),
+]
+
+articles = Article.select().join(ArticlePost).where(*expressions).order_by(fn.Random()).limit(48)
+
+now = int(time.time())
+
+feed = {"feed": [{"cursor": now-n, "post": f"at://{bsky.did}/app.bsky.feed.post/{a.articlepost_set[0].post_id}"} for n,a in enumerate(articles)]}
+
+FILENAME = "/tmp/random-feed.json"
+
+open(FILENAME, "w").write(json.dumps(feed))
+
+cmd = ["aws", "--quiet", "s3", "cp", FILENAME, f"s3://{S3_BUCKET}/{S3_PREFIX}/random.json"]
+subprocess.check_call(cmd)
